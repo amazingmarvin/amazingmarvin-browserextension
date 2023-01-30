457593cf65722462b5e61523182d2ad7038b57e2
Branimir Rijavec <rijavecbranko@gmail.com>
2023-01-30 17:24:52 +0200

    style bottom menu

--- src/components/BottomMenu.js
+++ src/components/BottomMenu.js
@@ -2,25 +2,27 @@ import { HiSun, HiPlusCircle, HiCog6Tooth } from "react-icons/hi2";
 
 const BottomMenu = ({ activeTab, setActiveTab }) => {
   return (
-    <div className="btm-nav">
-      <button
-        onClick={() => setActiveTab("today")}
-        className={`${activeTab === "today" ? "active bg-primary text-white" : "text-primary"}`}
-      >
-        <HiSun size="20" />
-        <span className="btm-nav-label">Today</span>
-      </button>
-      <button
-        onClick={() => setActiveTab("add-task")}
-        className={`${activeTab === "add-task" ? "active bg-primary text-white" : "text-primary"}`}
-      >
-        <HiPlusCircle size="20" />
-        <span className="btm-nav-label">Add Task</span>
-      </button>
-      <button className="text-primary">
-        <HiCog6Tooth size="20" />
-        <span className="btm-nav-label">Settings</span>
-      </button>
+    <div className="h-16">
+      <div className="btm-nav">
+        <button
+          onClick={() => setActiveTab("today")}
+          className={`${activeTab === "today" ? "active bg-primary text-white" : "text-primary"}`}
+        >
+          <HiSun size="20"/>
+          <span className="btm-nav-label">Today</span>
+        </button>
+        <button
+          onClick={() => setActiveTab("add-task")}
+          className={`${activeTab === "add-task" ? "active bg-primary text-white" : "text-primary"}`}
+        >
+          <HiPlusCircle size="20"/>
+          <span className="btm-nav-label">Add Task</span>
+        </button>
+        <button className="text-primary">
+          <HiCog6Tooth size="20"/>
+          <span className="btm-nav-label">Settings</span>
+        </button>
+      </div>
     </div>
   );
 };

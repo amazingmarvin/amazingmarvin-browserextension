0e0294bab6208c0ecbfa2037f9fd0105181e7668
Branimir Rijavec <rijavecbranko@gmail.com>
2023-02-06 18:45:02 +0200

    add loading spinner

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,18 +2,18 @@ import { getTasks } from "../utils/api";
 import Task from "./Task";
 import TaskListHeader from "./TaskListHeader";
 import { useEffect, useState } from "react";
-import { displayDate } from "../utils/dates";
+import LoadingSpinner from "./LoadingSpinner";
 
 const TaskList = ({ apiToken }) => {
   console.log(apiToken);
   const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [day, setDay] = useState(new Date());
 
   useEffect(() => {
-    console.log("api token", apiToken);
     getTasks(apiToken, day).then((tasks) => {
-      console.log("response in useffect", tasks);
       setTasks(tasks);
+      setIsLoading(false);
     });
   }, [day]);
 
@@ -23,9 +23,20 @@ const TaskList = ({ apiToken }) => {
 
   return (
     <div className="overflow-scroll scrollbar-hide">
-      <TaskListHeader day={day} setDay={setDay} />
-      <h2>{displayDate(day)}</h2>
-      <ul>{renderedTasks}</ul>
+      <TaskListHeader day={day} setDay={setDay} setIsLoading={setIsLoading} />
+      {isLoading ? (
+        <div className="h-64 grid place-content-center p-8">
+          <LoadingSpinner />
+        </div>
+      ) : tasks.length ? (
+        <ul>{renderedTasks}</ul>
+      ) : (
+        <div className="h-64 grid place-content-center p-8">
+          <h2 className="text-lg">
+            You don't have any tasks scheduled for this day.
+          </h2>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/TaskListHeader.js
+++ src/components/TaskListHeader.js
@@ -1,13 +1,20 @@
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import { displayDate, changeDay } from "../utils/dates";
 
-const TaskListHeader = ({ day, setDay }) => {
+const TaskListHeader = ({ day, setDay, setIsLoading }) => {
+  const handleClick = (direction) => {
+    setIsLoading(true);
+    direction === "right"
+      ? setDay(changeDay(day, 1))
+      : setDay(changeDay(day, -1));
+  };
+
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
         <button
           className="btn btn-ghost btn-circle"
-          onClick={() => setDay(changeDay(day, -1))}
+          onClick={() => handleClick("left")}
         >
           <HiChevronLeft size={20} />
         </button>
@@ -16,7 +23,10 @@ const TaskListHeader = ({ day, setDay }) => {
         <p className="btn btn-ghost normal-case text-l">{displayDate(day)}</p>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle" onClick={() => setDay(changeDay(day, 1))}>
+        <button
+          className="btn btn-ghost btn-circle"
+          onClick={() => handleClick("right")}
+        >
           <HiChevronRight size={20} />
         </button>
       </div>

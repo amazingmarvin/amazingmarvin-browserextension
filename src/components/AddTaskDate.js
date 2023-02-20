53a2a56c189d34962f4d40bd2a4ecc48aa73cd42
Branimir Rijavec <rijavecbranko@gmail.com>
2023-02-20 20:58:22 +0200

    create reusable component for adding dates
    create reusable component so that it can be used both for schedule and due dates and possible start and end dates if those options get added in the future

---
+++ src/components/AddTaskDate.js
@@ -0,0 +1,40 @@
+import RadioButton from "./RadioButton";
+
+const AddTaskDate = ({ type, date, buttons }) => {
+  const radioButtons = buttons.map((button) => {
+    if (button.hasOwnProperty("isDatePicker")) {
+      return (
+        <RadioButton
+          key={button.value}
+          value={button.value}
+          onClick={button.onClick}
+          onChange={button.onChange}
+        >
+          {button.icon}
+        </RadioButton>
+      );
+    }
+
+    return (
+      <RadioButton
+        key={button.value}
+        value={date === button.value}
+        onChange={button.onChange}
+      >
+        {button.icon}
+      </RadioButton>
+    );
+  });
+
+  return (
+    <div>
+      <label className="label">
+        <span className="label-text text-neutral">{type}</span>
+      </label>
+
+      <div className="flex flex-row gap-3 px-1">{radioButtons}</div>
+    </div>
+  );
+};
+
+export default AddTaskDate;

1f281a7e606aef1648b82da9bbe31001ec9d499d
Branimir Rijavec <rijavecbranko@gmail.com>
2023-02-20 20:59:41 +0200

    reorganize imports and refactor part for schedule dates

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,72 +1,68 @@
-import RadioButton from "./RadioButton";
-import { HiSun, HiOutlineMoon, HiCalendar } from "react-icons/hi2";
 import { useState } from "react";
 import { formatDate, isValidDate } from "../utils/dates";
+
+import { HiCalendar, HiOutlineMoon, HiSun } from "react-icons/hi2";
 import { BsCalendarX } from "react-icons/bs";
 
 import AddTaskTitle from "./AddTaskTitle";
+import AddTaskDate from "./AddTaskDate";
+
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
-import '../day-picker.css';
+import "../day-picker.css";
 
 const AddTask = () => {
-  const [taskTitle, setTaskTitle] = useState("")
+  const [taskTitle, setTaskTitle] = useState("");
+
   const [scheduleDate, setScheduleDate] = useState("unassigned");
   const [selected, setSelected] = useState(new Date());
   const [datePickerVisible, setDatePickerVisible] = useState(false);
 
+  const scheduleDateButtons = [
+    {
+      value: "today",
+      icon: <HiSun size={22} />,
+      onChange: () => {
+        setScheduleDate("today");
+      }
+    },
+    {
+      value: "tomorrow",
+      icon: <HiOutlineMoon size={22} />,
+      onChange: () => {
+        setScheduleDate("tomorrow");
+      },
+    },
+    {
+      isDatePicker: true,
+      value: isValidDate(scheduleDate),
+      icon: <HiCalendar size={22} />,
+      onClick: () => {
+        setDatePickerVisible(!datePickerVisible);
+      },
+      onChange: () => {
+        setDatePickerVisible(!datePickerVisible);
+      },
+    },
+    {
+      value: "unassigned",
+      icon: <BsCalendarX size={20} />,
+      onChange: () => {
+        setScheduleDate("unassigned");
+      }
+    },
+  ];
+
   return !datePickerVisible ? (
     <div className="form-control w-full p-5 gap-4">
-
       <AddTaskTitle title={taskTitle} setTaskTitle={setTaskTitle} />
 
+      <AddTaskDate
+        type="Schedule date"
+        date={scheduleDate}
+        buttons={scheduleDateButtons}
+      />
 
-      <div>
-        <label className="label">
-          <span className="label-text text-neutral">Schedule date</span>
-        </label>
-
-        <div className="flex flex-row gap-3 px-1">
-          <RadioButton
-            value={scheduleDate === "today"}
-            onChange={() => {
-              setScheduleDate("today");
-            }}
-          >
-            <HiSun size={22} />
-          </RadioButton>
-
-          <RadioButton
-            value={scheduleDate === "tomorrow"}
-            onChange={() => {
-              setScheduleDate("tomorrow");
-            }}
-          >
-            <HiOutlineMoon size={22} />
-          </RadioButton>
-
-          <RadioButton
-            value={isValidDate(scheduleDate)}
-            onClick={() => {
-              setDatePickerVisible(!datePickerVisible);
-            }}
-            onChange={() => {
-              setDatePickerVisible(!datePickerVisible);
-            }}
-          >
-            <HiCalendar size={22} />
-          </RadioButton>
-
-          <RadioButton
-            value={scheduleDate === "unassigned"}
-            onChange={() => {
-              setScheduleDate("unassigned");
-            }}
-          >
-            <BsCalendarX size={20} />
-          </RadioButton>
-        </div>
-      </div>
       <button className="btn btn-primary text-white">Create Task</button>
     </div>
   ) : (
@@ -80,8 +76,24 @@ const AddTask = () => {
           setDatePickerVisible(false);
         }}
       />
-      <button className="btn btn-circle btn-outline btn-sm absolute top-2 right-2" onClick={() => setDatePickerVisible(false)}>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+      <button
+        className="btn btn-circle btn-outline btn-sm absolute top-2 right-2"
+        onClick={() => setDatePickerVisible(false)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
       </button>
     </div>
   );

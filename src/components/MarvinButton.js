119bef340e0a86b4271b26c47b5fbc993e7b8d65
Branimir Rijavec <rijavecbranko@gmail.com>
2023-03-07 11:09:29 +0200

    refactor marvin button
    add the textSize prop

--- src/components/MarvinButton.js
+++ src/components/MarvinButton.js
@@ -1,9 +1,11 @@
-const MarvinButton = ({ children, ...rest }) => {
+const MarvinButton = ({ children, textSize, ...rest }) => {
+  textSize = textSize || "text-sm";
+
   return (
     <button
       type="button"
       {...rest}
-      className="text-white bg-gradient-to-r from-[#26d6c4] to-[#10b1d3] hover:bg-gradient-to-br focus:outline-none shadow-lg shadow-[0_2px_10px_rgba(28,197,203,0.5)] font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-1 my-1"
+      className={`text-white bg-gradient-to-r from-[#26d6c4] to-[#10b1d3] hover:bg-gradient-to-br focus:outline-none shadow-lg shadow-[0_2px_10px_rgba(28,197,203,0.5)] font-medium rounded-lg ${textSize} text-center px-5 py-2.5 mx-1 my-1`}
     >
       {children}
     </button>

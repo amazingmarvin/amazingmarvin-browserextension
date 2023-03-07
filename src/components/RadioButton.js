0e57864afe06ea5bafaff154de58291adbe824e7
Branimir Rijavec <rijavecbranko@gmail.com>
2023-03-07 11:35:10 +0200

    change cursor to pointer on hover

--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -1,10 +1,16 @@
 const RadioButton = ({ children, value, onChange, onClick }) => {
   return (
-    <label>
-      <input type="radio" checked={value} onChange={onChange} onClick={onClick} className="hidden"/>
-      <div className={value ? 'text-primary' : ''}>{children}</div>
+    <label className="hover:cursor-pointer">
+      <input
+        type="radio"
+        checked={value}
+        onChange={onChange}
+        onClick={onClick}
+        className="hidden"
+      />
+      <div className={value ? "text-primary" : ""}>{children}</div>
     </label>
   );
 };
 
-export default RadioButton
+export default RadioButton;

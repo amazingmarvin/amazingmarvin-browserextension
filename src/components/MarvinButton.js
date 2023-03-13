dfc0e38fd70e489f150b1095a59dc96a901b9fb8
Branimir Rijavec <rijavecbranko@gmail.com>
2023-03-13 11:50:56 +0200

    add width and disabled props to MarvinButton

--- src/components/MarvinButton.js
+++ src/components/MarvinButton.js
@@ -1,11 +1,12 @@
-const MarvinButton = ({ children, textSize, ...rest }) => {
+const MarvinButton = ({ children, textSize, width, disabled, ...rest }) => {
   textSize = textSize || "text-sm";
-
+  width = width ? "w-full" : "";
   return (
     <button
       type="button"
+      className={`${width} disabled:opacity-70 disabled:cursor-not-allowed text-white bg-gradient-to-r from-[#26d6c4] to-[#10b1d3] hover:bg-gradient-to-br focus:outline-none shadow-lg shadow-[0_2px_10px_rgba(28,197,203,0.5)] font-medium rounded-lg ${textSize} text-center px-5 py-2.5 mx-1 my-1`}
+      disabled={disabled}
       {...rest}
-      className={`text-white bg-gradient-to-r from-[#26d6c4] to-[#10b1d3] hover:bg-gradient-to-br focus:outline-none shadow-lg shadow-[0_2px_10px_rgba(28,197,203,0.5)] font-medium rounded-lg ${textSize} text-center px-5 py-2.5 mx-1 my-1`}
     >
       {children}
     </button>

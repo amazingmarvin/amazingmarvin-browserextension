37cc08ab95388bc8cee57b3bedfad35f7bff4521
Branimir Rijavec <rijavecbranko@gmail.com>
2023-07-04 11:19:50 +0300

    Add buttons for clearing task title and note.

--- src/popup/components/AddTaskNote.js
+++ src/popup/components/AddTaskNote.js
@@ -1,9 +1,24 @@
+import { BsX } from "react-icons/bs";
+
 const AddTaskNote = ({ note, setNote }) => {
   return (
     <div>
-      <label className="label">
-        <span className="label-text text-neutral">Task note</span>
-      </label>
+      <div className="flex flex-row items-center gap-0.5">
+        <label className="label">
+          <span className="label-text text-neutral">Task note</span>
+        </label>
+        {note.length > 0 && (
+          <button
+            type="button"
+            className="relative btn btn-xs btn-ghost btn-circle no-animation text-red-500 mb-[0.15rem]"
+            data-hov="clear note"
+            data-pos="C"
+            onClick={() => setNote("")}
+          >
+            <BsX size={18} />
+          </button>
+        )}
+      </div>
       <textarea
         placeholder="Task note..."
         rows={1}
--- src/popup/components/AddTaskTitle.js
+++ src/popup/components/AddTaskTitle.js
@@ -1,9 +1,24 @@
+import { BsX } from "react-icons/bs";
+
 const AddTaskTitle = ({ title, setTaskTitle }) => {
   return (
     <div>
-      <label className="label">
-        <span className="label-text text-neutral">Task title</span>
-      </label>
+      <div className="flex flex-row items-center gap-0.5">
+        <label className="label">
+          <span className="label-text text-neutral">Task title</span>
+        </label>
+        {title.length > 0 && (
+          <button
+            type="button"
+            className="relative btn btn-xs btn-ghost btn-circle no-animation text-red-500 mb-[0.15rem]"
+            data-hov="clear title"
+            data-pos="C"
+            onClick={() => setTaskTitle("")}
+          >
+            <BsX size={18} />
+          </button>
+        )}
+      </div>
       <input
         type="text"
         value={title}

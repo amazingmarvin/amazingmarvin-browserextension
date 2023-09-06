4894c7d1212ef25abd9c72347c2cd335afbfa42d
Mark Tucker <mark@amazingmarvin.com>
2023-09-06 15:12:30 +0200

    Fix prop names for react.

--- src/options/components/OptionsContentGeneral.js
+++ src/options/components/OptionsContentGeneral.js
@@ -140,9 +140,9 @@ const OptionsContentGeneral = () => {
                 >
                   <path
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="m1 1 4 4 4-4"
                   />
                 </svg>
